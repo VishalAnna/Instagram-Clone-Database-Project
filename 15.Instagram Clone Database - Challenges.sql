4da2eb42f094bac814a11b61b8e080ff63da64b0
Vishal Nivangune <95202004+VishalAnna@users.noreply.github.com>
2023-08-14 23:03:54 +0530

    Update 15.Instagram Clone Database - Challenges.sql

--- 15.Instagram Clone Database - Challenges.sql
+++ 15.Instagram Clone Database - Challenges.sql
@@ -13,6 +13,7 @@ GROUP BY 1
 ORDER BY 2 DESC;
 
 /*version 2*/
+
 SELECT 
     DAYNAME(created_at) AS day,
     COUNT(*) AS total
@@ -24,6 +25,7 @@ LIMIT 2;
 
 /*We want to target our inactive users with an email campaign.
 Find the users who have never posted a photo*/
+
 SELECT username
 FROM users
 LEFT JOIN photos ON users.id = photos.user_id
@@ -32,6 +34,7 @@ WHERE photos.id IS NULL;
 
 /*We're running a new contest to see who can get the most likes on a single photo.
 WHO WON??!!*/
+
 SELECT users.username,photos.id,photos.image_url,COUNT(*) AS Total_Likes
 FROM likes
 JOIN photos ON photos.id = likes.photo_id
@@ -41,6 +44,7 @@ ORDER BY Total_Likes DESC
 LIMIT 1;
 
 /*version 2*/
+
 SELECT 
     username,
     photos.id,
